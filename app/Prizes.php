dd0862383a0065fd4cc9d53edcd7793b9695c221
pnechushkin <pnechushkin@gmail.com>
2019-03-20 23:58:23 +0200

    random prize

--- app/Prizes.php
+++ app/Prizes.php
@@ -2,29 +2,9 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Prizes {
-
-	protected static $prises_type = [
-		'money',
-		'points',
-		'object',
-	];
-
-	protected static $prises_list = [
-		'prise1',
-		'prise2',
-		'prise3',
-		'prise4',
-		'prise5',
-		'prise6',
-	];
-
-	protected static $money_limit = [ 50, 500 ];
-	protected static $points_limit = [ 250, 1000 ];
-
-	protected static $quality_money = 10;
-	protected static $quality_points = 50;
-
-
+class Prizes extends Model
+{
+    protected $table ='users_prizes';
 }
