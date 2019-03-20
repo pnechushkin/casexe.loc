69330cb333fd14923b0fec70071cbe7fac937e5e
pnechushkin <pnechushkin@gmail.com>
2019-03-20 22:14:39 +0200

    auth users and seeds BD

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		for ( $i = 0; $i <= 100; $i ++ ) {
+			DB::table( 'users' )->insert( [
+				'name'     => Str::random( 10 ),
+				'email'    => Str::random( 10 ) . '@gmail.com',
+				'password' => bcrypt( 'secret' ),
+			] );
+		}
+	}
+}
