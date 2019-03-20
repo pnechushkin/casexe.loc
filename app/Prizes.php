213313bc740149deb71ae0701d6aff6d08c6b63a
pnechushkin <pnechushkin@gmail.com>
2019-03-20 23:04:32 +0200

    fixed migrations BD

--- app/Prizes.php
+++ app/Prizes.php
@@ -5,4 +5,26 @@ namespace App;
 
 class Prizes {
 
+	protected static $prises_type = [
+		'money',
+		'points',
+		'object',
+	];
+
+	protected static $prises_list = [
+		'prise1',
+		'prise2',
+		'prise3',
+		'prise4',
+		'prise5',
+		'prise6',
+	];
+
+	protected static $money_limit = [ 50, 500 ];
+	protected static $points_limit = [ 250, 1000 ];
+
+	protected static $quality_money = 10;
+	protected static $quality_points = 50;
+
+
 }
--- database/migrations/2019_03_20_191113_create_users_prises_table.php
+++ database/migrations/2019_03_20_191113_create_users_prises_table.php
@@ -15,7 +15,8 @@ class CreateUsersPrisesTable extends Migration
     {
         Schema::create('users_prizes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
+	        $table->integer('user_id')->unsigned();
+	        $table->foreign('user_id')->references('id')->on('users');
             $table->string('prize');
             $table->string('action');
             $table->string('status');
